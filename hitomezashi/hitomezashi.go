6e34d08e0db48f886d9d29617a6b93fb8197bbd3
Rich Middleditch <rich@witchert.co.uk>
2021-12-22 09:31:25 +0000

    Move color configuration into  package

--- hitomezashi/hitomezashi.go
+++ hitomezashi/hitomezashi.go
@@ -3,17 +3,21 @@ package hitomezashi
 import (
 	"image/color"
 	"math"
-	"math/rand"
-	"strconv"
 
 	"github.com/fogleman/gg"
-	"github.com/muesli/gamut"
+)
+
+type ColorChoice int
+
+const (
+	COLOR_A ColorChoice = 0
+	COLOR_B ColorChoice = 1
 )
 
 type Cell struct {
-	color      color.Color
-	rightBound bool
-	topBound   bool
+	colorChoice ColorChoice
+	rightBound  bool
+	topBound    bool
 }
 
 type Hitomezashi struct {
@@ -25,119 +29,97 @@ type Hitomezashi struct {
 	across      []string
 	down        []string
 	cells       []Cell
-	boxColorA   color.Color
-	boxColorB   color.Color
+	colorA      color.Color
+	colorB      color.Color
 }
 
-var colors = []color.Color{
-	color.NRGBA64{57818, 35930, 12168, 65535},
-	color.NRGBA64{19216, 48673, 16633, 65535},
-	color.NRGBA64{57090, 34948, 47660, 65535},
-	color.NRGBA64{36003, 45435, 13660, 65535},
-	color.NRGBA64{20404, 39496, 20721, 65535},
-	color.NRGBA64{52060, 15473, 58198, 65535},
-	color.NRGBA64{46641, 36500, 57269, 65535},
-	color.NRGBA64{57480, 15399, 30954, 65535},
-	color.NRGBA64{42716, 42029, 24161, 65535},
-	color.NRGBA64{39231, 22794, 37294, 65535},
-	color.NRGBA64{55575, 38184, 27239, 65535},
-	color.NRGBA64{16859, 42870, 53708, 65535},
-	color.NRGBA64{41379, 27522, 9102, 65535},
-	color.NRGBA64{49700, 24602, 29228, 65535},
-	color.NRGBA64{55766, 29478, 56914, 65535},
-	color.NRGBA64{42260, 24550, 16624, 65535},
-	color.NRGBA64{28496, 23990, 58742, 65535},
-	color.NRGBA64{47353, 17967, 10505, 65535},
-	color.NRGBA64{58946, 25652, 10518, 65535},
-	color.NRGBA64{20285, 45089, 37379, 65535},
-	color.NRGBA64{59253, 28945, 25262, 65535},
-	color.NRGBA64{55792, 14417, 47691, 65535},
-	color.NRGBA64{57658, 13672, 10787, 65535},
-	color.NRGBA64{27779, 29712, 10939, 65535},
-	color.NRGBA64{27078, 27904, 54256, 65535},
-	color.NRGBA64{33328, 15023, 59635, 65535},
-	color.NRGBA64{40308, 19184, 46614, 65535},
-	color.NRGBA64{27566, 39157, 59264, 65535},
-	color.NRGBA64{55481, 14389, 20760, 65535},
-	color.NRGBA64{50442, 41923, 13432, 65535},
-	color.NRGBA64{23731, 27990, 45070, 65535},
-	color.NRGBA64{52365, 18426, 38505, 65535},
-}
+func New(horizontal []string, vertical []string, scale int) Hitomezashi {
+	h := Hitomezashi{
+		across:      horizontal,
+		down:        vertical,
+		width:       len(horizontal),
+		height:      len(vertical),
+		scale:       scale,
+		borderWidth: 24.0,
+		colorA:      color.Black,
+		colorB:      color.White,
+	}
 
-func New(horizontal []string, vertical []string, scale int, hash string) Hitomezashi {
-	h := Hitomezashi{across: horizontal, down: vertical, width: len(horizontal), height: len(vertical), scale: scale, borderWidth: 24}
+	pixelWidth := h.width*h.scale + h.borderWidth
+	pixelHeight := h.height*h.scale + h.borderWidth
+	h.Canvas = gg.NewContext(pixelWidth, pixelHeight)
 
-	// Set up colors
-	seed, _ := strconv.ParseInt(hash[:7], 16, 64)
-	r := rand.New(rand.NewSource(seed))
-	h.boxColorA = colors[r.Intn(len(colors)-1)+1]
-	h.boxColorB = gamut.Complementary(h.boxColorA)
-
-	h.cells = make([]Cell, h.width*h.height)
-
-	h.setupCanvas()
-
-	h.calculateCells()
-
-	h.drawLines()
-
-	h.drawBorder()
+	h.setupCells()
 
 	return h
 }
 
-func (h *Hitomezashi) setupCanvas() {
-	pixelWidth := h.width*h.scale + h.borderWidth
-	pixelHeight := h.height*h.scale + h.borderWidth
-	h.Canvas = gg.NewContext(pixelWidth, pixelHeight)
+func (h *Hitomezashi) SetColors(a color.Color, b color.Color) {
+	h.colorA = a
+	h.colorB = b
+}
+
+func (h *Hitomezashi) Draw() {
+	h.drawCanvas()
+
+	h.fillCells()
+	h.drawLines()
+
+	h.drawCanvasBorder()
+}
+
+func (h *Hitomezashi) drawCanvas() {
 	h.Canvas.SetHexColor("#ffffff")
 	h.Canvas.Clear()
 }
 
-func (h *Hitomezashi) calculateCells() {
+func (h *Hitomezashi) setupCells() {
 	for row := 0; row < h.height; row++ {
 		for col := 0; col < h.width; col++ {
-			position := (row * h.width) + col
-			cell := Cell{color: h.boxColorA, topBound: false, rightBound: false}
+			cell := Cell{colorChoice: COLOR_A, topBound: false, rightBound: false}
 
-			cell.determineCellBounds(row, col, h)
-			cell.determineCellColor(position, h)
+			cell.setCellBounds(row, col, h)
+			cell.setCellColor(row, col, h)
 
-			// Draw the boxes...
-			x := col*h.scale + (h.borderWidth / 2)
-			y := row*h.scale + (h.borderWidth / 2)
-
-			h.Canvas.SetColor(cell.color)
-			h.Canvas.DrawRectangle(float64(x), float64(y), float64(h.scale), float64(h.scale))
-			h.Canvas.Fill()
-
-			h.cells[position] = cell
+			h.cells = append(h.cells, cell)
 		}
 	}
 }
 
+func (h *Hitomezashi) fillCells() {
+	for index, cell := range h.cells {
+		col := index % h.width
+		row := index / h.width
+		x := col*h.scale + (h.borderWidth / 2)
+		y := row*h.scale + (h.borderWidth / 2)
+
+		h.Canvas.SetColor(h.getColorFromChoice(cell.colorChoice))
+		h.Canvas.DrawRectangle(float64(x), float64(y), float64(h.scale), float64(h.scale))
+		h.Canvas.Fill()
+	}
+}
+
 func (h *Hitomezashi) drawLines() {
-	// Set up line drawing
+	// Set line width based on the scale
 	h.Canvas.SetLineWidth(math.RoundToEven(float64(h.scale / 6)))
 
-	// Draw lines
-	for pos, cell := range h.cells {
+	for index, cell := range h.cells {
 		if cell.topBound || cell.rightBound {
-			col := pos % h.width
-			row := pos / h.width
-			x := int(col)*h.scale + (h.borderWidth / 2)
-			y := int(row)*h.scale + (h.borderWidth / 2)
+			col := index % h.width
+			row := index / h.width
+			x := col*h.scale + (h.borderWidth / 2)
+			y := row*h.scale + (h.borderWidth / 2)
 
-			h.Canvas.SetHexColor("#000000")
+			h.Canvas.SetRGB(0, 0, 0)
 
-			// Draw the horizontal lines...
-			if cell.topBound && pos >= h.width {
+			// Horizontal line
+			if cell.topBound && index >= h.width {
 				h.Canvas.DrawLine(float64(x), float64(y), float64(x+h.scale), float64(y))
 				h.Canvas.Stroke()
 			}
 
-			// Draw the vertical lines...
-			if cell.rightBound && pos%h.width < h.width-1 {
+			// Vertical line
+			if cell.rightBound && index%h.width < h.width-1 {
 				h.Canvas.DrawLine(float64(x+h.scale), float64(y), float64(x+h.scale), float64(y+h.scale))
 				h.Canvas.Stroke()
 			}
@@ -145,49 +127,48 @@ func (h *Hitomezashi) drawLines() {
 	}
 }
 
-func (h *Hitomezashi) drawBorder() {
+func (h *Hitomezashi) drawCanvasBorder() {
 	h.Canvas.SetLineWidth(float64(h.borderWidth))
-	h.Canvas.SetHexColor("#ffffff")
+	h.Canvas.SetRGB(1, 1, 1)
 	h.Canvas.DrawRectangle(0, 0, float64(h.Canvas.Width()), float64(h.Canvas.Height()))
 	h.Canvas.Stroke()
 }
 
-func (h *Hitomezashi) oppositeColor(color color.Color) color.Color {
-	if color == h.boxColorA {
-		return h.boxColorB
+func (h *Hitomezashi) getColorFromChoice(choice ColorChoice) color.Color {
+	if choice == COLOR_A {
+		return h.colorA
 	}
-	return h.boxColorA
+	return h.colorB
 }
 
-func (cell *Cell) determineCellBounds(row int, col int, h *Hitomezashi) {
-	// Figure out if bounded on top
+func (cell *Cell) setCellBounds(row int, col int, h *Hitomezashi) {
 	if (h.down[row] == "0" && col%2 != 0) || (h.down[row] == "1" && col%2 == 0) {
 		cell.topBound = true
 	}
 
-	// Figure out if bounded on right
 	if (h.across[col] == "0" && row%2 != 0) || (h.across[col] == "1" && row%2 == 0) {
 		cell.rightBound = true
 	}
 }
 
-func (cell *Cell) determineCellColor(position int, h *Hitomezashi) {
+func (cell *Cell) setCellColor(row int, col int, h *Hitomezashi) {
+	position := (row * h.width) + col
 	if position > 0 {
-		// If we are on the cell starting a new row then look to the row above
+		// Starting cell in a row, use color from row above
 		if position%h.width == 0 {
 			// is our cell top bound, if so use opposite color
 			if cell.topBound {
-				cell.color = h.oppositeColor(h.cells[position-h.width].color)
+				cell.colorChoice = ^h.cells[position-h.width].colorChoice
 				// our cell isnt top bound so match the cell aboves color
 			} else {
-				cell.color = h.cells[position-h.width].color
+				cell.colorChoice = h.cells[position-h.width].colorChoice
 			}
 			// otherwise look to the previous cell to see if its right bound
 		} else if h.cells[position-1].rightBound {
-			cell.color = h.oppositeColor(h.cells[position-1].color)
+			cell.colorChoice = ^h.cells[position-1].colorChoice
 			// previous cell isnt bound so match its color
 		} else {
-			cell.color = h.cells[position-1].color
+			cell.colorChoice = h.cells[position-1].colorChoice
 		}
 	}
 }
--- main.go
+++ main.go
@@ -3,19 +3,58 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"image/color"
 	"image/png"
+	"math/rand"
 	"net/http"
+	"strconv"
 	"strings"
 
 	"github.com/fogleman/gg"
 	"github.com/go-git/go-git/v5"
 	"github.com/go-git/go-git/v5/config"
 	"github.com/go-git/go-git/v5/storage/memory"
+	"github.com/muesli/gamut"
 
 	"github.com/witchert/hitomezashi/hitomezashi"
 )
 
 const MAX_SIZE = 257
+
+var colors = []color.Color{
+	color.NRGBA64{57818, 35930, 12168, 65535},
+	color.NRGBA64{19216, 48673, 16633, 65535},
+	color.NRGBA64{57090, 34948, 47660, 65535},
+	color.NRGBA64{36003, 45435, 13660, 65535},
+	color.NRGBA64{20404, 39496, 20721, 65535},
+	color.NRGBA64{52060, 15473, 58198, 65535},
+	color.NRGBA64{46641, 36500, 57269, 65535},
+	color.NRGBA64{57480, 15399, 30954, 65535},
+	color.NRGBA64{42716, 42029, 24161, 65535},
+	color.NRGBA64{39231, 22794, 37294, 65535},
+	color.NRGBA64{55575, 38184, 27239, 65535},
+	color.NRGBA64{16859, 42870, 53708, 65535},
+	color.NRGBA64{41379, 27522, 9102, 65535},
+	color.NRGBA64{49700, 24602, 29228, 65535},
+	color.NRGBA64{55766, 29478, 56914, 65535},
+	color.NRGBA64{42260, 24550, 16624, 65535},
+	color.NRGBA64{28496, 23990, 58742, 65535},
+	color.NRGBA64{47353, 17967, 10505, 65535},
+	color.NRGBA64{58946, 25652, 10518, 65535},
+	color.NRGBA64{20285, 45089, 37379, 65535},
+	color.NRGBA64{59253, 28945, 25262, 65535},
+	color.NRGBA64{55792, 14417, 47691, 65535},
+	color.NRGBA64{57658, 13672, 10787, 65535},
+	color.NRGBA64{27779, 29712, 10939, 65535},
+	color.NRGBA64{27078, 27904, 54256, 65535},
+	color.NRGBA64{33328, 15023, 59635, 65535},
+	color.NRGBA64{40308, 19184, 46614, 65535},
+	color.NRGBA64{27566, 39157, 59264, 65535},
+	color.NRGBA64{55481, 14389, 20760, 65535},
+	color.NRGBA64{50442, 41923, 13432, 65535},
+	color.NRGBA64{23731, 27990, 45070, 65535},
+	color.NRGBA64{52365, 18426, 38505, 65535},
+}
 
 // Convert a given string to binary representation for each rune
 func stringToBinary(str string) string {
@@ -44,8 +83,7 @@ func hashFromLSRemote(owner string, repo string, branch string) (string, error) 
 	return hash, nil
 }
 
-func generateImage(owner string, repo string, branch string) (*gg.Context, error) {
-	size := 280
+func generateImage(owner string, repo string, branch string, size int) (*gg.Context, error) {
 	if size >= MAX_SIZE {
 		size = MAX_SIZE
 	}
@@ -54,15 +92,22 @@ func generateImage(owner string, repo string, branch string) (*gg.Context, error
 	if err != nil {
 		return nil, err
 	}
-	hash = stringToBinary(hash)
-
-	acrossBinary := hash[:size]
-	downBinary := hash[len(hash)-(size):]
+	hashBinary := stringToBinary(hash)
+	acrossBinary := hashBinary[:size]
+	downBinary := hashBinary[len(hashBinary)-(size):]
 
 	across := strings.Split(acrossBinary, "")
 	down := strings.Split(downBinary, "")
 
-	h := hitomezashi.New(across, down, 12, hash)
+	h := hitomezashi.New(across, down, 12)
+
+	seed, _ := strconv.ParseInt(hash[:7], 16, 64)
+	r := rand.New(rand.NewSource(seed))
+	colorA := colors[r.Intn(len(colors)-1)+1]
+	colorB := gamut.Complementary(colorA)
+	h.SetColors(colorA, colorB)
+
+	h.Draw()
 
 	return h.Canvas, nil
 }
@@ -74,7 +119,8 @@ func handler(w http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	canvas, err := generateImage(req.URL.Query().Get("owner"), req.URL.Query().Get("repo"), req.URL.Query().Get("branch"))
+	size, _ := strconv.Atoi(req.URL.Query().Get("size"))
+	canvas, err := generateImage(req.URL.Query().Get("owner"), req.URL.Query().Get("repo"), req.URL.Query().Get("branch"), size)
 	if err != nil {
 		fmt.Fprintln(w, err)
 		return
